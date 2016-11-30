feccfdd3da35d1a66256537e57b7c5f1fa17261d
Aaron Goldstein <bredmold@gmail.com>
2016-11-30 11:11:52 -0700

    Updated and improved documentation

--- docs/silver-dollar.rb
+++ docs/silver-dollar.rb
@@ -1,41 +1,38 @@
 # Example silver-dollar.rb configuration file
 {
+    # Configuration section - affecting how silver-dollar operates
     config: {
+        # Your Git clones live here
         project_dir: File.expand_path('~/Projects'),
+
+        # List of branches that silver-dollar knows about by default
         pull_branches: ['master', 'develop'],
+
+        # Default basis branch for rebase operations
         basis_branch: 'develop',
     },
+
+    # Projects section - these are the projects you want to build
     projects: [
+        # This project always builds, regardless of SCM state
         {library: 'shutdown-services', always_build: true},
+
+        # Ordinary library build
         {library: 'config'},
-        {library: 'authentication'},
-        {library: 'bizops-parent'},
-        {library: 'alg-ota-support'},
-        {library: 'uds-metrics-cdi'},
-        {library: 'uap-common'},
-        {library: 'alg-error'},
-        {library: 'alg-domain'},
-        {library: 'alg-test-utils'},
-        {library: 'ari-client'},
-        {library: 'alg-common-data'},
-        {library: 'alg-data', mvn_flags: ['-Pclean-db']},
-        {library: 'alg-data-load', mvn_flags: ['-Pautopatch']},
-        {library: 'booking-data', repo: 'core'},
-        {library: 'pricing-data'},
-        {library: 'ari-data'},
-        {library: 'bizops-client'},
-        {library: 'hbsi'},
-        {library: 'verticalbooking', repo: 'core'},
-        {library: 'pricing-client'},
-        {service: 'bizops-service', archive: 'bizops.war'},
-        {service: 'bizops', archive: 'alg.war'},
-        {service: 'ari-service', archive: 'ari.war'},
-        {service: 'geo', wlp: 'core', restart: false},
+
+        # Ordinary library build with custom Maven options
+        {library: 'data', mvn_flags: ['-Pclean-db']},
+
+        # Library build that is a Maven sub-project in a higher-level repository
+        {library: 'sub-project', repo: 'core'},
+
+        # Simple service build with stock assumptions
+        {service: 'service'},
+
+        # Service build with custom archive name
+        {service: 'foo-service', archive: 'foo.war'},
+
+        # Service build that changes some deployment properties (likely to change)
         {service: 'product', wlp: 'core', restart: false},
-        {service: 'hotel', wlp: 'core', restart: false},
-        {service: 'pricing', restart: false},
-        {library: 'trip'},
-        {library: 'package-svc'},
-        {library: 'user'},
     ]
 }
